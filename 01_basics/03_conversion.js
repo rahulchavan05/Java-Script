3a382649f6b287e7de8539562e55e43910bdf318
rahulchavan05 <103823430+rahulchavan05@users.noreply.github.com>
2024-10-04 17:40:25 +0000

    03_conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,42 @@
+let scoure = "30"
+
+//console.log(typeof scoure)
+
+let valueInNumbers = Number(scoure)
+
+//console.log(typeof valueInNumbers)
+//console.log(valueInNumbers)
+
+// If variable value is not a number it will print NAN
+
+//console.log("If variable value is not a number it will print NAN")
+
+let scoure2 = "30abc"
+
+//console.log(typeof scoure2)
+
+let valueInNumbers2 = Number(scoure2)
+
+//console.log(typeof valueInNumbers2)
+//console.log(valueInNumbers2)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "Rahul"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+// 1  => true; 0=> false
+// "" => false
+//"name" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(someNumber)
+console.log(typeof someNumber)
