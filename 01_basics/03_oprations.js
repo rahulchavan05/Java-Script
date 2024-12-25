217bf38143c59695776b0a490f0bd2b8e9c3f500
rahulchavan05 <103823430+rahulchavan05@users.noreply.github.com>
2024-12-25 09:43:53 +0000

    day 3

--- 01_basics/03_oprations.js
+++ 01_basics/03_oprations.js
@@ -1,3 +1,14 @@
 let value = 3
 let negValue = -value
-cpnsole.log(negValue);
+//console.log(negValue);
+
+let num, num2, num3
+num = num2 =num3 = 2+2
+
+let gameCounter = 100 
+gameCounter++;
+//console.log(gameCounter);
+
+let x =3;
+const y = x++;
+console.log(`x:${x} ,y:${y}`);
