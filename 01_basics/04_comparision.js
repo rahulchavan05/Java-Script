92bc7f9c0dda0a7e85bc60aadfcb5096a7b08cf8
rahulchavan05 <103823430+rahulchavan05@users.noreply.github.com>
2024-12-25 10:26:59 +0000

    data typr summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+//console.log(2>1);
+
+//two deffrent data type comparision 
+
+//console.log("2" > 1);                 // true
+
+//console.log(null > 0);                // false
+//console.log(null == 0);               // false
+//console.log(null >= 0);               // true
+
+
+console.log(undefined > 0);             // false
+console.log(undefined < 0);             // false
+
+// srick check  "===="
+
+console.log("2" === 2);                 // false
+
+ 
