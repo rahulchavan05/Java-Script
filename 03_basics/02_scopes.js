73e866a3ce6f4cb687facfe8eee2e13e85b0a9b7
rahulchavan05 <103823430+rahulchavan05@users.noreply.github.com>
2025-01-28 17:06:26 +0000

    02_scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,6 +12,57 @@ if (true){
 
 // console.log(a);
 // console.log(b);
-console.log(c);   // var will print the value out of scope 
+// console.log(c);   // var will print the value out of scope 
+
+
+function one(){
+    const username = "rahul"
+
+    function two(){
+        const wesite = "youtube"
+        console.log(username)
+    }
+    // console.log(wesite)
+
+    two()
+
+
+
+}
+
+//one()
+
+ if (true) {
+    const username = "rahul"
+    if(username === "rahul") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+
+    // console.log(website)
+ }
+
+ // console.log(username);
+
+
+ //++++++++++++++++++++ interasting +++++++++++++++++++++
+
+ console.log(addOne(5))
+
+ function addOne(num){
+    return num + 1
+ }
+ 
+ console.log(addTwo(5));
+ 
+ const addTwo = function(num){
+    return num + 2
+ }
+
+ console.log(addTwo(5));
+ 
+
+
 
 
