70832b26a5fe8b8aa0995cf0ea76d39a79db7e47
rahulchavan05 <103823430+rahulchavan05@users.noreply.github.com>
2025-01-28 14:20:09 +0000

    02_scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+
+// let a = 10
+// const b = 20
+var c = 300
+
+if (true){
+
+    let a = 10
+    const b = 20
+     c = 30              // If not data type give to variable it will consider as a var
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);   // var will print the value out of scope 
+
+
